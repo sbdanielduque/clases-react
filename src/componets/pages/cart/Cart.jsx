bb933e644df3e79a913ee9d463f884b7425701aa
Daniel Noguera <sbdanielduque@gmail.com>
2024-05-20 15:46:18 -0400

    carrito con renderizado

--- src/componets/pages/cart/Cart.jsx
+++ src/componets/pages/cart/Cart.jsx
@@ -1,28 +1,38 @@
-import { Button } from "@mui/material"
+import { Button, IconButton } from "@mui/material"
 import React from "react"
 import { Link } from "react-router-dom"
+import DeleteIcon from "@mui/icons-material/Delete"
 
 const Cart = ({ cart, deleteItem, total, clearCartAlert }) => {
     return (
         <div>
-            <h1>Este es el carrito</h1>
+            <h1>Your Cart</h1>
             {cart.map((product) => (
                 <div key={product.id} style={{ border: "solid 1px black" }}>
                     <h3>{product.name}</h3>
-                    <h3>{product.price}</h3>
+                    <h3>{product.price.toFixed(2)}</h3>
                     <h3>{product.quantity}</h3>
-                    <button onClick={() => deleteItem(product.id)}>
-                        eliminar
-                    </button>
+                    <IconButton
+                        onClick={() => deleteItem(product.id)}
+                        aria-label="delete"
+                        color="error"
+                    >
+                        <DeleteIcon />
+                    </IconButton>
                 </div>
             ))}
-            <div>el total a pagar es: {total}</div>
-            <Link to={"/checkout"}>
-                <Button variant="contained">Finalizar compra</Button>
-            </Link>
-            <Button onClick={clearCartAlert} variant="outlined">
-                Limpiar Carrito
-            </Button>
+            {cart.length > 0 && (
+                <>
+                    <div>your total is: {total}</div>
+
+                    <Link to={"/checkout"}>
+                        <Button variant="contained">Checkout</Button>
+                    </Link>
+                    <Button onClick={clearCartAlert} variant="outlined">
+                        Clear cart
+                    </Button>
+                </>
+            )}
         </div>
     )
 }
